9548d64432412b6875c872ec93d2c6b12a71be62
Eliton Albino <eliton.albino@hotmail.com>
2026-05-28 21:12:43 -0300

    correção de apontamento

--- src/main/java/com/javanauta/agendadortarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -1,5 +1,6 @@
 package com.javanauta.agendadortarefas.infrastructure.client;
 
+import com.javanauta.agendadortarefas.business.dto.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "usuario", url = "${usuario.url}")
 public interface UsuarioClient {
-    @GetMapping
+    @GetMapping("/usuario")
     UsuarioDTO buscaUsuarioPorEmail(@RequestParam("email") String email,
                                     @RequestParam("Authorization") String token);
 
